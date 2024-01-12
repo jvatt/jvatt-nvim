8207385b5ca6b0841bff3b3c187df7ca3513ee44
Jalmari Vattulainen <jalmari.vattulainen@digia.com>
2024-01-12 17:47:24 +0200

    Neo-tree auto expand

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,5 +6,10 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  }
+  },
+  config = function()
+    require("neo-tree").setup({
+      auto_expand_width = true
+    })
+  end
 }
