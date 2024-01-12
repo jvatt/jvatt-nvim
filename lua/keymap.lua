0ce88e10d97b0203e20c8152510848ef61cb45dc
Jalmari Vattulainen <jalmari.vattulainen@digia.com>
2024-01-12 17:48:53 +0200

    LSP keybinds

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -6,3 +6,7 @@ vim.keymap.set('n', '<leader>s', builtin.find_files, {})
 vim.keymap.set('n', '<leader>f', builtin.live_grep, {})
 -- Space + e
 vim.keymap.set('n','<leader>e', ':Neotree filesystem reveal left<CR>')
+-- Shift + k
+vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+-- gd
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
