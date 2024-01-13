c7e6f11736482956d64a03d69665cd16357b04ce
Jalmari <jalmari.vattulainen@gmail.com>
2024-01-13 03:43:33 +0200

    Treesitter auto install

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,12 +2,11 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
-    local config = require('nvim-treesitter.configs')
+    local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"vim", "vimdoc", "lua", "javascript", "typescript", "tsx", "yaml"},
+      auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
     })
-  end
+  end,
 }
-
