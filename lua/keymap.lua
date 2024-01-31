9da9496fa91388354a08e6a49b5a3db9a998d7a0
Jalmari Vattulainen <jalmari.vattulainen@digia.com>
2024-01-31 13:19:06 +0200

    Gitsigns keybinds

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,11 +1,15 @@
--- Telescope --
+-- Telescope -------
 local builtin = require("telescope.builtin")
 -- Space + s
 vim.keymap.set("n", "<leader>s", builtin.find_files, {})
 -- Space + f
 vim.keymap.set("n", "<leader>f", builtin.live_grep, {})
+
+-- Neotree ---------
 -- Space + e
 vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left<CR>")
+
+-- LSP -------------
 -- Shift + k
 vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 -- gd
@@ -14,3 +18,8 @@ vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 -- gf
 vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+
+-- Gitsigns --------
+-- Space + gp
+vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
