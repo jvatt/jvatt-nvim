5f9a160a4b68c7ba6d53232dd646dfa2c7520f2f
Jalmari Vattulainen <jalmari.vattulainen@digia.com>
2024-03-12 09:37:25 +0200

    Fix none-ls deprecated builtins

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,12 +1,24 @@
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
   config = function()
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
+        require("none-ls.diagnostics.eslint"),
+        require("none-ls.code_actions.eslint"),
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.diagnostics.codespell.with({
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "typescript",
+            "typescriptreact",
+            "graphql" }
+        }),
       },
     })
   end,
