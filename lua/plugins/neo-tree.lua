a8d366615038d751b7872fed0b7471419aab7cfd
Jalmari Vattulainen <jalmari.vattulainen@digia.com>
2026-02-04 19:07:53 +0200

    Neo-tree: Show line number

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,41 +1,42 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  },
-  config = function()
-    require("neo-tree").setup({
-      -- Disable mouse
-      window = {
-        mappings = {
-          ["<LeftMouse>"] = "noop",
-          ["<2-LeftMouse>"] = "noop", -- Disables double-click to open
-        },
-      },
-      -- Show relative line numbers
-      event_handlers = {
-        {
-          event = "neo_tree_buffer_enter",
-          handler = function()
-            vim.cmd([[
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
+	config = function()
+		require("neo-tree").setup({
+			-- Disable mouse
+			window = {
+				mappings = {
+					["<LeftMouse>"] = "noop",
+					["<2-LeftMouse>"] = "noop", -- Disables double-click to open
+				},
+			},
+			-- Show relative line numbers
+			event_handlers = {
+				{
+					event = "neo_tree_buffer_enter",
+					handler = function()
+						vim.cmd([[
           setlocal relativenumber
+          setlocal number
         ]])
-          end,
-        },
-      },
+					end,
+				},
+			},
 
-      auto_expand_width = true,
-      filesystem = {
-        filtered_items = {
-          visible = true,
-          hide_dotfiles = false,
-          hide_gitignored = true,
-        },
-      },
-    })
-  end,
+			auto_expand_width = true,
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					hide_dotfiles = false,
+					hide_gitignored = true,
+				},
+			},
+		})
+	end,
 }
