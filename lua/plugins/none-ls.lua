8723fdec68df246a16e116b380f935f882ca6aa3
Jalmari Vattulainen <jalmari.vattulainen@digia.com>
2026-02-09 11:45:59 +0200

    Delete graphql from config

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,45 +1,44 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
-  config = function()
-    local null_ls = require("null-ls")
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-    null_ls.setup({
-      --debug = true,
-      sources = {
-        require("none-ls.diagnostics.eslint_d"),
-        require("none-ls.code_actions.eslint_d"),
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.codespell.with({
-          filetypes = {
-            "javascript",
-            "javascriptreact",
-            "typescript",
-            "typescriptreact",
-            "graphql",
-          },
-        }),
-      },
-      on_attach = function(client, bufnr)
-        if client.name == "null-ls" and client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({
-                bufnr = bufnr,
-                filter = function(_client)
-                  return _client.name ~= "ts_ls"
-                end,
-              })
-            end,
-          })
-        end
-      end,
-    })
-  end,
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+		null_ls.setup({
+			--debug = true,
+			sources = {
+				require("none-ls.diagnostics.eslint_d"),
+				require("none-ls.code_actions.eslint_d"),
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.diagnostics.codespell.with({
+					filetypes = {
+						"javascript",
+						"javascriptreact",
+						"typescript",
+						"typescriptreact",
+					},
+				}),
+			},
+			on_attach = function(client, bufnr)
+				if client.name == "null-ls" and client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({
+								bufnr = bufnr,
+								filter = function(_client)
+									return _client.name ~= "ts_ls"
+								end,
+							})
+						end,
+					})
+				end
+			end,
+		})
+	end,
 }
